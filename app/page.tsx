f40f39eee19ab8bd2ba5b4b6fac04e5b5d2b5c54
Marcos Faria <marcos.faria19@hotmail.com>
2024-08-05 15:17:14 -0300

    Add shadcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
-import Image from "next/image"
+import { Button } from "./_components/ui/button"
 
 export default function Home() {
-  return <button className="bg-red m-10 text-right text-red-200">Opa</button>
+  return (
+    <>
+      <button className="bg-red m-10 text-right text-red-200">Opa</button>
+      <Button>Opa2</Button>
+    </>
+  )
 }
